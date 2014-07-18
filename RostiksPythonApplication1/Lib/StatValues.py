1d890a2d74752ed568c2e088797ed3f7a64265c2
ryavorsky <ryavorsky@gmail.com>
2014-07-18 09:33:34 +0400

    Stat summary

--- RostiksPythonApplication1/Lib/StatValues.py
+++ RostiksPythonApplication1/Lib/StatValues.py
@@ -5,8 +5,10 @@ def computeValues(subFolder, statData) :
     computeBossTeacher(subFolder)
     computeManWomen(subFolder, statData)
     computeAge(subFolder, statData)
-
-    return
+    computeEducation(subFolder, statData)
+    computeWorkYears(subFolder, statData)
+    computeWorkHereYears(subFolder, statData)
+    computeTeachCat(subFolder, statData)
 
 def extractAnswers(statData, questionNumbers) :
     res = []
@@ -71,3 +73,81 @@ def computeAge(subFolder, statData) :
     BuildTex.addMacros(subFolder, 'numMidAge', str(mid))
     BuildTex.addMacros(subFolder, 'numSenior', str(senior))
     BuildTex.addMacros(subFolder, 'numOld', str(old))
+
+def computeEducation(subFolder, statData) :
+    values = extractAnswers(statData, [15])
+    eduValues = []
+    for v in values :
+        v = v.replace('{','').replace('}','')
+        eduList = v.split('=')
+        for e in eduList :
+            if len(e) > 1 :
+                eduValues.append(e)
+
+    keys = [str(i+16) for i in range(7)]
+    eduStat = [eduValues.count(key) for key in keys]
+    print 'Education values:', eduValues, eduStat
+    BuildTex.addMacros(subFolder, 'numEduA', str(eduStat[0]))
+    BuildTex.addMacros(subFolder, 'numEduB', str(eduStat[1]))
+    BuildTex.addMacros(subFolder, 'numEduC', str(eduStat[2]))
+    BuildTex.addMacros(subFolder, 'numEduD', str(eduStat[3]))
+    BuildTex.addMacros(subFolder, 'numEduE', str(eduStat[4]))
+    BuildTex.addMacros(subFolder, 'numEduF', str(eduStat[5]))
+    BuildTex.addMacros(subFolder, 'numEduG', str(eduStat[6]))
+
+def computeWorkYears(subFolder, statData) :
+    values = extractAnswers(statData, [16])
+    years = map(extractNumber, values)
+    print '\nExperience:', values, years
+    [expA, expB, expC, expD] = [0,0,0,0]
+    for y in years :
+        if y < 5 :
+            expA += 1
+        elif y < 11 :
+            expB += 1
+        elif y < 21 :
+            expC += 1
+        else:
+            expD += 1 
+    BuildTex.addMacros(subFolder, 'numExpA', str(expA))
+    BuildTex.addMacros(subFolder, 'numExpB', str(expB))
+    BuildTex.addMacros(subFolder, 'numExpC', str(expC))
+    BuildTex.addMacros(subFolder, 'numExpD', str(expD))
+
+def extractNumber(s) :
+    try :
+        res = int(s[0:2])
+    except Exception:
+        res = int(s[0:1])
+
+    return res
+
+def computeWorkHereYears(subFolder, statData):
+    values = extractAnswers(statData, [17])
+    years = map(extractNumber, values)
+    print '\nExperience:', values, years
+    [expA, expB, expC, expD] = [0,0,0,0]
+    for y in years :
+        if y < 5 :
+            expA += 1
+        elif y < 11 :
+            expB += 1
+        elif y < 21 :
+            expC += 1
+        else:
+            expD += 1 
+    BuildTex.addMacros(subFolder, 'numExpHereA', str(expA))
+    BuildTex.addMacros(subFolder, 'numExpHereB', str(expB))
+    BuildTex.addMacros(subFolder, 'numExpHereC', str(expC))
+    BuildTex.addMacros(subFolder, 'numExpHereD', str(expD))
+
+def computeTeachCat(subFolder, statData) :
+    values = extractAnswers(statData, [19])
+    keys = [str(i+55) for i in range(5)]
+    stat = [values.count(key) for key in keys]
+    print '\nTeacher categories:', values, stat
+    BuildTex.addMacros(subFolder, 'numTechCatA', str(stat[0]))
+    BuildTex.addMacros(subFolder, 'numTechCatB', str(stat[1]))
+    BuildTex.addMacros(subFolder, 'numTechCatC', str(stat[2]))
+    BuildTex.addMacros(subFolder, 'numTechCatD', str(stat[3]))
+    BuildTex.addMacros(subFolder, 'numTechCatE', str(stat[4]))
